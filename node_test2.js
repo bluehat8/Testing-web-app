f58118f37f6e075cc9e3a4c3e01d348211e9b91c
Agustín Amaya <80552327+bluehat8@users.noreply.github.com>
2022-12-12 14:14:30 -0600

    Update node_test2.js
    Corrección de llaves e invocación de la función

--- node_test2.js
+++ node_test2.js
@@ -6,18 +6,20 @@ function searchTextOnGoogle(){
     var driver= new webdriver.Builder().forBrowser('chrome').build();
 
     //Ir a una url
-    driver.get('https://wwww.google.com').then(function()
+    driver.get('https://wwww.lambdatest.com').then(function()
     {
       driver.findElement(webdriver.By.linkText('Automation')).click().then(function()
       {
-        driver.getTitle().then(funcion(title))
+        driver.getTitle().then(function(title)
         {
             setTimeout(function()
             {
                 console.log(title);
                 driver.quit();
             },5000);
-        }
-      })
+        })
+      });
     });
 }
+
+searchTextOnGoogle();
